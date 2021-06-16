1a0176dda79c1af8954395df6ce3cd85d62da068
denisa <timisdenisaioana@yahoo.com>
2021-06-16 21:25:38 +0300

    api done

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+ini_set('display_errors', false);
 require '../vendor/autoload.php';
 $app = new \Slim\App;
 
@@ -62,6 +62,55 @@ $app->get('/user/{id}', function (Request $request, Response $response, array $a
             ->withStatus(500);
     }
 });
+
+/**
+ * @OA\Get(
+ *     path="/profileStory/{userId}",
+ *     tags={"stories"},
+ *     summary="Gets all stories from data base by user id",
+ *     @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          description="Id of the user who wrote the stories",
+ *          required=true,
+ *          @OA\Schema( type="int")
+ *     ),
+ *     @OA\Response(response="200", description="Successfully got stories by user id!"),
+ *     @OA\Response(response="404", description="Stories not found!")    
+ * )
+ */
+$app->get('/profileStory/{userId}', function (Request $request, Response $response, array $args) {
+    require_once '../db/dbconn.php';
+
+    $id = $args['userId'];
+    $sql_statement = $pdo->prepare("SELECT story.title, user.first_name, user.last_name, paragraph.text FROM story,user,paragraph WHERE story.user_id=:userId AND user.id=story.user_id AND story.id=paragraph.story_id");
+
+    try{
+        $sql_statement->execute(['userId' => $id]); 
+        $story = $sql_statement->fetch();
+        $response->getBody()->write(json_encode(array(
+            'title' => $story[0],
+            'first_name' => $story[1],
+            'last_name' => $story[2],
+            'text' => $story[3]
+        )
+        ));
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(200);
+    }catch(PDOException $e)
+    {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
 
 /**
  * @OA\Get(
@@ -227,7 +276,31 @@ try{
 //____________________________________________________________________________________________________
 //POST
 
-//register user
+/**
+ * @OA\Post(
+ *     path="/register",
+ *     tags={"user"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"first_name","last_name","username","password","cpassword","gender","dob","country","type"},
+ *              @OA\Property(property="first_name" , type="string"),
+ *              @OA\Property(property="last_name" , type="string"),
+ *              @OA\Property(property="username" , type="string"),
+ *              @OA\Property(property="password" , type="string"),
+ *              @OA\Property(property="cpassword" , type="string"),
+ *              @OA\Property(property="gender" , type="string"),
+ *              @OA\Property(property="dob" , type="string"),
+ *              @OA\Property(property="country" , type="string"),
+ *              @OA\Property(property="type" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully registered user!"),
+ *     @OA\Response(response="404", description="Register failed!") 
+ * )
+ */
 $app->post('/register', function( Request $request, Response $response){
 
     require_once '../db/dbconn.php';
@@ -281,7 +354,27 @@ $app->post('/register', function( Request $request, Response $response){
     }
 });
 
-//add review
+
+
+/**
+ * @OA\Post(
+ *     path="/review/add",
+ *     tags={"review"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"storyId","criticId","text"},
+ *              @OA\Property(property="storyId" , type="int"),
+ *              @OA\Property(property="criticId" , type="int"),
+ *              @OA\Property(property="text" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully added review!"),
+ *     @OA\Response(response="404", description="Something went wrong while adding review!") 
+ * )
+ */
 $app->post('/review/add', function( Request $request, Response $response){
 
     require_once '../db/dbconn.php';
@@ -322,10 +415,111 @@ $app->post('/review/add', function( Request $request, Response $response){
     }
 });
 
+/**
+ * @OA\Post(
+ *     path="/story/add",
+ *     tags={"stories"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"userId","title","text","type"},
+ *              @OA\Property(property="userId" , type="int"),
+ *              @OA\Property(property="title" , type="string"),
+ *              @OA\Property(property="text" , type="string"),
+ *              @OA\Property(property="type" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully added story!"),
+ *     @OA\Response(response="404", description="Something went wrong while adding story!") 
+ * )
+ */
+$app->post('/story/add', function( Request $request, Response $response){
+
+    require_once '../db/dbconn.php';
+    $sql_statementS = $pdo->prepare("SELECT MAX(id)
+                                    FROM story");
+    $sql_statementP = $pdo->prepare("SELECT MAX(id)
+                                    FROM paragraph");
+    $sql_statementS->execute(); 
+    $newId = $sql_statementS->fetch()[0];
+    $newId = $newId + 1;
+
+    $sql_statementP->execute(); 
+    $newIdP = $sql_statementP->fetch()[0];
+    $newIdP=$newIdP + 1;
+
+    $postDate=date("d.m.Y");
+
+    $title = $request->getParam('title');
+    $type = $request->getParam('type');
+    $text = $request->getParam('text');
+    $userId = $request->getParam('userId');
+
+
+    $sqlS = $pdo->prepare("INSERT INTO story (ID,TITLE,USER_ID,TYPE,POST_DATE, DISLIKES,LIKES,VIEWS) 
+            VALUES(:newId,:title,:userId,:type,:postDate,0,0,0)");
+  
+    try {
+      $sqlS->bindParam(':newId', $newId);
+      $sqlS->bindParam(':title', $title);
+      $sqlS->bindParam(':type', $type);
+      $sqlS->bindParam(':userId', $userId);
+      $sqlS->bindParam(':postDate', $postDate);
+      $resultS = $sqlS->execute();
+
+      $pageOrder=1;
+      $paragraphs=explode("\n",$text);
+        foreach($paragraphs as $paragraph)
+        {
+            $sqlP = $pdo->prepare("INSERT INTO paragraph (ID,STORY_ID,PAGE_ORDER,TEXT) 
+            VALUES(:newIdP,:newId,:pageOrder,:text)");
+              $sqlP->bindParam(':newIdP', $newIdP);
+              $sqlP->bindParam(':newId', $newId);
+              $sqlP->bindParam(':pageOrder', $pageOrder);
+              $sqlP->bindParam(':text', $paragraph);
+
+              $resultP = $sqlP->execute();
+              $pageOrder=$pageOrder+1;
+              $newIdP=$newIdP + 1;
+        }
+      $response->getBody()->write(json_encode($resultS));
+      return $response
+        ->withHeader('content-type','application/json')
+        ->withStatus(200);
+
+    } catch( PDOException $e ) {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+    
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
+
 //____________________________________________________________________________________________________
 //DELETE
 
-//delete review
+/**
+ * @OA\Delete(
+ *     path="/deletereview/{id}",
+ *     tags={"review"},
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          description="Id of the review we want to delete",
+ *          required=true,
+ *          @OA\Schema( type="int")
+ *     ),
+ *     @OA\Response(response="200", description="Successfully deleted review!"),
+ *     @OA\Response(response="404", description="Review not found!") 
+ * )
+ */
 $app->delete('/deletereview/{id}', function( Request $request, Response $response, array $args){
 
     require_once '../db/dbconn.php';
@@ -353,10 +547,114 @@ $app->delete('/deletereview/{id}', function( Request $request, Response $respons
     }
 });
 
+/**
+ * @OA\Delete(
+ *     path="/deletestory/{id}",
+ *     tags={"stories"},
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          description="Id of the story we want to delete",
+ *          required=true,
+ *          @OA\Schema( type="int")
+ *     ),
+ *     @OA\Response(response="200", description="Successfully deleted story!"),
+ *     @OA\Response(response="404", description="Story not found!") 
+ * )
+ */
+$app->delete('/deletestory/{id}', function( Request $request, Response $response, array $args){
+
+    require_once '../db/dbconn.php';
+    
+    $id=$args['id'];
+    $sqlP = $pdo->prepare("DELETE FROM paragraph WHERE paragraph.story_id=:storyId");
+    $sqlS = $pdo->prepare("DELETE FROM story WHERE id=:storyId");
+  
+    try {
+      $sqlS->bindParam(':storyId', $id);
+      $sqlP->bindParam(':storyId', $id);
+      $resultP = $sqlP->execute();
+      $resultS = $sqlS->execute();
+      $response->getBody()->write(json_encode( $resultP));
+
+      return $response
+        ->withHeader('content-type','application/json')
+        ->withStatus(200);
+
+    } catch( PDOException $e ) {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+    
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
+/**
+ * @OA\Delete(
+ *     path="/deleteuser/{id}",
+ *     tags={"user"},
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          description="Id of the user we want to delete",
+ *          required=true,
+ *          @OA\Schema( type="int")
+ *     ),
+ *     @OA\Response(response="200", description="Successfully deleted user!"),
+ *     @OA\Response(response="404", description="User not found!") 
+ * )
+ */
+$app->delete('/deleteuser/{id}', function( Request $request, Response $response, array $args){
+
+    require_once '../db/dbconn.php';
+    
+    $id=$args['id'];
+    $sql = $pdo->prepare("DELETE FROM user WHERE id=:userId");
+  
+    try {
+      $sql->bindParam(':userId', $id);
+      $result = $sql->execute();
+      $response->getBody()->write(json_encode($result));
+      return $response
+        ->withHeader('content-type','application/json')
+        ->withStatus(200);
+
+    } catch( PDOException $e ) {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+    
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
 //____________________________________________________________________________________________________
 //PUT
 
-//edit review
+/**
+ * @OA\Put(
+ *     path="/editreview/{id}",
+ *     tags={"review"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"text"},
+ *              @OA\Property(property="text" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully edited review!"),
+ *     @OA\Response(response="404", description="Review not found!") 
+ * )
+ */
 $app->put('/editreview/{id}', function( Request $request, Response $response, array $args){
 
     require_once '../db/dbconn.php';
@@ -389,6 +687,127 @@ $app->put('/editreview/{id}', function( Request $request, Response $response, ar
     }
 });
 
+/**
+ * @OA\Put(
+ *     path="/edituser/{id}",
+ *     tags={"user"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"first_name" ,"last_name" ,"gender","country"},
+ *              @OA\Property(property="first_name" , type="string"),
+ *              @OA\Property(property="last_name" , type="string"),
+ *              @OA\Property(property="gender" , type="string"),
+ *              @OA\Property(property="country" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully edited user!"),
+ *     @OA\Response(response="404", description="User not found!") 
+ * )
+ */
+$app->put('/edituser/{id}', function( Request $request, Response $response, array $args){
+
+    require_once '../db/dbconn.php';
+
+    $id=$args['id'];
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $gender = $request->getParam('gender');
+    $country = $request->getParam('country');
+    
+    $sql = $pdo->prepare("UPDATE user SET first_name=:first_name,last_name=:last_name,gender=:gender,country=:country WHERE id=:id");
+  
+    try {
+
+      $sql->bindParam(':id', $id);
+      $sql->bindParam(':first_name', $first_name);
+      $sql->bindParam(':last_name', $last_name);
+      $sql->bindParam(':gender', $gender);
+      $sql->bindParam(':country', $country);
+
+      $result = $sql->execute();
+      $response->getBody()->write(json_encode($result));
+      return $response
+        ->withHeader('content-type','application/json')
+        ->withStatus(200);
+
+    } catch( PDOException $e ) {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+    
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
+/**
+ * @OA\Put(
+ *     path="/editstory/{id}",
+ *     tags={"stories"},
+ *     @OA\RequestBody(
+ *       @OA\MediaType(
+ *          mediaType="json",
+ *          @OA\Schema( 
+ *              required={"text","type","title"},
+ *              @OA\Property(property="text" , type="string"),
+ *              @OA\Property(property="type" , type="string"),
+ *              @OA\Property(property="title" , type="string")
+ *              )
+ *          )
+ *     ),
+ *     @OA\Response(response="200", description="Successfully edited story!"),
+ *     @OA\Response(response="404", description="Story not found!") 
+ * )
+ */
+$app->put('/editstory/{id}', function( Request $request, Response $response, array $args){
+
+    require_once '../db/dbconn.php';
+
+    $id=$args['id'];
+    $title = $request->getParam('title');
+    $text = $request->getParam('text');
+    $type = $request->getParam('type');
+    
+    $sql = $pdo->prepare("UPDATE story SET title=:title,type=:type WHERE id=:id");
+  
+    try {
+
+      $sql->bindParam(':id', $id);
+      $sql->bindParam(':title', $title);
+      $sql->bindParam(':type', $type);
+
+      $result = $sql->execute();
+      $paragraphs=explode("\n",$text);
+        foreach($paragraphs as $paragraph)
+        {
+              $sqlP = $pdo->prepare("UPDATE paragraph SET text=:text WHERE id=:id)");
+              $sqlP->bindParam(':text', $paragraph);
+
+              $resultP = $sqlP->execute();
+        }
+
+      $response->getBody()->write(json_encode($result));
+      return $response
+        ->withHeader('content-type','application/json')
+        ->withStatus(200);
+
+    } catch( PDOException $e ) {
+        $error= array(
+            'message' => $e->getMessage()
+        );
+    
+        $response->getBody()->write($error);
+        return $response
+            ->withHeader('content-type','application/json')
+            ->withStatus(500);
+    }
+});
+
 //____________________________________________________________________________________________________
 //retun pages
 
@@ -396,7 +815,6 @@ $app->get('/', function (Request $request, Response $response, array $args)use($
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader);
     echo $twig->render('home.php', array(
-        'loggedUserId' => $loggedUserId
     ));
 });
 
@@ -404,7 +822,6 @@ $app->get('/create', function (Request $request, Response $response, array $args
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader);
     echo $twig->render('create-article.php', array(
-        'loggedUserId' =>$loggedUserId
     ));
 });
 
@@ -412,7 +829,6 @@ $app->get('/login', function (Request $request, Response $response, array $args)
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader);
     echo $twig->render('login.php', array(
-        'loggedUserId' => $loggedUserId
     ));
 });
 
@@ -420,7 +836,6 @@ $app->get('/signup', function (Request $request, Response $response, array $args
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader);
     echo $twig->render('signup.php', array(
-        'loggedUserId' => $GLOBALS['loggedUserId']
     ));
 });
 
